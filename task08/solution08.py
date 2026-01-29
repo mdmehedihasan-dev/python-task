107114f372834f616f50d60b1a985391e3d44d03
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2026-01-30 01:22:37 +0600

    task 09

---
+++ task08/solution08.py
@@ -0,0 +1,6 @@
+cpu_load = 0.88
+
+# Convert to percentage string
+cpu_percent = f"{cpu_load * 100:.0f}%"
+
+print(cpu_percent)
---
+++ task09/solution09.py
@@ -0,0 +1,9 @@
+import os
+
+base_dir = "/var/log"
+app_name = "nginx"
+filename = "access.log"
+
+full_path = os.path.join(base_dir, app_name, filename)
+
+print(full_path)
